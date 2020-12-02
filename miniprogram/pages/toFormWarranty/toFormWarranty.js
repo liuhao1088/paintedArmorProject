6fe590b68bf77b88e059e709f119c9a4f3bb7e15
liuhao1088 <1350569467@qq.com>
2020-12-02 18:01:14 +0800

    更新

--- miniprogram/pages/toFormWarranty/toFormWarranty.js
+++ miniprogram/pages/toFormWarranty/toFormWarranty.js
@@ -5,7 +5,114 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 头部信息
+    item: {
+      title: '质保录入信息',
+      msg: '漆面装甲，高端汽车漆面保护膜'
+    },
 
+    //年份
+    index: 0,
+    array: ['2020', '2021', '2022', '2023'],
+    dateArray: [{
+        id: 0,
+        name: '2020'
+      },
+      {
+        id: 1,
+        name: '2021'
+      },
+      {
+        id: 2,
+        name: '2022'
+      },
+      {
+        id: 3,
+        name: '2023'
+      }
+    ],
+    //品牌
+    brandIndex: 0,
+    brandArray: ['劳斯莱斯', '宝马', '保时捷', '宾利'],
+    objectBrandArray: [{
+        id: 0,
+        name: '劳斯莱斯'
+      },
+      {
+        id: 1,
+        name: '宝马'
+      },
+      {
+        id: 2,
+        name: '保时捷'
+      },
+      {
+        id: 3,
+        name: '宾利'
+      }
+    ],
+    //车型
+    modelIndex: 0,
+    modelArray: ['a16', 'b52', 'c86', 'y85'],
+    objectModelArray: [{
+        id: 0,
+        name: 'a16'
+      },
+      {
+        id: 1,
+        name: 'b52'
+      },
+      {
+        id: 2,
+        name: 'c86'
+      },
+      {
+        id: 3,
+        name: 'y85'
+      }
+    ],
+    imgUrl: '', //验收单
+  },
+  // 年份
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  // 品牌
+  bindBrandChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e)
+    this.setData({
+      brandIndex: e.detail.value
+    })
+  },
+  // 车型
+  bindModelChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e)
+    this.setData({
+      modelIndex: e.detail.value
+    })
+  },
+
+  //上传图片
+  insertImage() {
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success (res) {
+        console.log(res)
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+      }
+    })
+  },
+  toDetails(evevt){
+    
+    wx.navigateTo({
+      url: '../../pages/details/details',
+    })
   },
 
   /**
